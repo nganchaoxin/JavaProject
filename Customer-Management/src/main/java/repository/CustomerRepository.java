948acf124c148a87407f40a50ebdfb4b1ab95391
nganchaoxin <kimnganphamsu01@gmail.com>
2023-01-04 16:38:14 +0700

    Add service package and up query

--- Customer-Management/src/main/java/repository/CustomerRepository.java
+++ Customer-Management/src/main/java/repository/CustomerRepository.java
@@ -12,10 +12,12 @@ public interface CustomerRepository extends CrudRepository<CustomerEntity, Integ
     @Query(value = "select n from CustomerEntity n where n.name like ?1%")
     List<CustomerEntity> findByStartName(String name);
 
-    @Query(value = "Select * from CustomerEntity n where n.phone = ?", nativeQuery = true)
+    //@Query(value = "Select * from CustomerEntity n where n.phone = ?", nativeQuery = true)
     List<CustomerEntity> findByPhone(int phone);
 
     List<CustomerEntity> findByEmail(String email);
+
+    List<CustomerEntity> findByPhoneOrEmail(int phone, String email);
 
     @Query(value = "select * from CustomerEntity n where n.sex = 'male' and DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), birthdate)), '%Y') + 0  between 20 and 30", nativeQuery = true)
     List<CustomerEntity> listMen();
